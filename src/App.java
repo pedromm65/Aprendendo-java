1bbeb637a64eed81ec313630ff3763e5d13b7be2
pedromm65 <pedromm65@hotmail.com.br>
2022-05-11 19:33:35 -0300

    JAVA

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int idade;
+        idade = 37;
+
+        System.out.println(idade);
+
+        System.out.println("A idade é " + idade);
+    }
+}
