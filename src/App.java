81100dc0e07f89f7b4e9542f52b05dd958faffbe
pedromm65 <pedromm65@hotmail.com.br>
2022-05-12 20:34:59 -0300

    Aprendendo java

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        double salario = 1270.50;
-        int valor = (int) salario;
+        double valor1 = 0.2;
+        double valor2 = 0.1;
 
-        System.out.println(valor);
+        double total = valor1 + valor2;
+
+        System.out.println(total);
     }
 }
