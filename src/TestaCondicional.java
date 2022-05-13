1ef3b73dcf62bcae4d05159fa8bcbca9c12cd680
pedromm65 <pedromm65@hotmail.com.br>
2022-05-13 15:17:44 -0300

    Aprendendo Java

--- src/TestaCondicional.java
+++ src/TestaCondicional.java
@@ -1,9 +1,14 @@
 public class TestaCondicional {
     public static void main(String[] args) {
-        int idade = 20;
+        int idade = 16;
+        int quantidadePessoas = 3;
 
         if (idade >= 18) {
             System.out.println("Voce tem mais de 18 anos");
+        } else if (quantidadePessoas >= 2){
+            System.out.println("Voce não tem 18 mas pode entrar");
+        } else {
+            System.out.println("Voce não pode entrar");
         }
     }
 }
