8bdfcb7289fbcd925cea349be23d200dc46d9d73
pedromm65 <pedromm65@hotmail.com.br>
2022-05-13 15:13:58 -0300

    Aprendendo jgava

---
+++ src/TestaCondicional.java
@@ -0,0 +1,9 @@
+public class TestaCondicional {
+    public static void main(String[] args) {
+        int idade = 20;
+
+        if (idade >= 18) {
+            System.out.println("Voce tem mais de 18 anos");
+        }
+    }
+}
